ab3d438225d7536b75c1060d066fbb21f188f55a
karthickpandiyan <pandiyan289@gmail.com>
2019-12-31 18:24:07 +0530

    [Created Test case] Given 2019 is not leap year and it should return false

--- app/src/test/java/com/kp/findleapyear/LeapYearTest.kt
+++ app/src/test/java/com/kp/findleapyear/LeapYearTest.kt
@@ -1,5 +1,17 @@
 package com.kp.findleapyear
 
+import org.junit.Assert
+import org.junit.Test
+
 class LeapYearTest {
 
+
+    @Test
+    fun `Should find the given year is not leap year, YEAR = 2019`(){
+        Assert.assertFalse(isLeapYear(2019))
+    }
+
+    private fun isLeapYear(year: Int): Boolean {
+        return year == 2008
+    }
 }
