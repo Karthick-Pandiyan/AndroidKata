1a637d31ae795de8e28f858c3a7e7bdfbe3a6123
karthickpandiyan <pandiyan289@gmail.com>
2020-01-01 10:14:53 +0530

    Created LeapYear class and Refactor to encapsulate the behaviour and make it the solution usable

---
+++ app/src/main/java/com/kp/findleapyear/LeapYear.kt
@@ -0,0 +1,12 @@
+package com.kp.findleapyear
+
+class LeapYear(private val year: Int) {
+
+    private fun isLeapYear(): Boolean {
+        return isDivisibleBy4ButNotBy100() || isDivisibleBy400()
+    }
+
+    private fun isDivisibleBy400() = year % 400 == 0
+
+    private fun isDivisibleBy4ButNotBy100() = year % 4 == 0 && year % 100 != 0
+}
